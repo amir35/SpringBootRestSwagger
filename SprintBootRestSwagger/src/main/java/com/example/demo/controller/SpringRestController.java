c4bf88a94baaa1ae776d45e6f7f9f2f3af50986b
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2023-01-02 18:41:56 +0530

    Updating Controller

--- SprintBootRestSwagger/src/main/java/com/example/demo/controller/SpringRestController.java
+++ SprintBootRestSwagger/src/main/java/com/example/demo/controller/SpringRestController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +29,13 @@ public class SpringRestController {
 	{
 		return "Welcome to Spring Boot Rest";
 	}
-	
+
+	@ApiOperation(value = "getCustomer", notes="Get All customer list",nickname = "Customer List")
+	@ApiResponses(value = {
+			@ApiResponse(code = 500, message = "Server error"),
+			@ApiResponse(code = 404, message = "Service not found"),
+			@ApiResponse(code = 200, message = "Successful retrieval",
+					response = SpringRestController.class, responseContainer = "List") })
 	@GetMapping("/customer")
 	public List<CustomerEntity> getCustomer()
 	{
